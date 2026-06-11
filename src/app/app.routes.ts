52aa99b69d604c432b7e54f39394dac3beacd672
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2026-06-11 20:00:27 -0300

    feat: Criação das páginas Login e Cadastro e fazendo suas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login').then(m => m.Login)
+    },
+    {
+    path: 'cadastro',
+    loadComponent: () =>
+      import('./pages/cadastro/cadastro').then(m => m.Cadastro)
+    }
+];

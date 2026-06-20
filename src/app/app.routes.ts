06c7a2cd9b87d0352fe9f75a56d56e964f9adc46
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2026-06-20 16:21:47 -0300

    feat: Criação inicial da página de login e cadastro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-    {
+  {
+    path: '',
+    redirectTo: 'cadastro',
+    pathMatch: 'full',
+  },
+  {
     path: 'login',
-    loadComponent: () =>
-      import('./pages/login/login').then(m => m.Login)
-    },
-    {
+    loadComponent: () => import('./pages/login/login').then((m) => m.Login),
+  },
+  {
     path: 'cadastro',
-    loadComponent: () =>
-      import('./pages/cadastro/cadastro').then(m => m.Cadastro)
-    }
+    loadComponent: () => import('./pages/cadastro/cadastro').then((m) => m.Cadastro),
+  },
 ];

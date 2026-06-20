8fdc8f732bd3addd3f59df9a4fc126f18ad86ee6
Jonas Pinheiro <jonas.pinheiro@aluno.ifsp.edu.br>
2026-06-20 17:39:37 -0300

    feat: Fazendo a tela de cadastro

--- src/app/pages/cadastro/cadastro.ts
+++ src/app/pages/cadastro/cadastro.ts
@@ -1,9 +1,60 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './cadastro.html',
   styleUrls: ['./cadastro.scss'],
 })
-export class Cadastro {}
+export class Cadastro {
+  form: FormGroup;
+  erro: string = '';
+  mostrarSenha: boolean = false;
+  mostrarConfirmarSenha: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+  ) {
+    this.form = fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', Validators.required],
+      confirmarSenha: ['', Validators.required],
+    });
+  }
+
+  exibirSenha() {
+    this.mostrarSenha = !this.mostrarSenha;
+  }
+
+  exibirConfirmarSenha() {
+    this.mostrarConfirmarSenha = !this.mostrarConfirmarSenha;
+  }
+
+  onSubmit() {
+    if (this.form.invalid) return;
+
+    const { nome, email, senha, confirmarSenha } = this.form.value;
+
+    if (senha !== confirmarSenha) {
+      this.erro = 'As senhas estão diferentes';
+      return;
+    }
+
+    const cliente = { nome, email, senha, saldo: 0 };
+
+    this.authService.cadastrar(cliente).subscribe({
+      next: () => {
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        this.erro = err.error?.message || 'Erro ao cadastrar';
+      },
+    });
+  }
+}
